33dea472bc6499fc1ca0f7883023a37da82cc11d
Perla D. Canario Perez <perladenis163@gmail.com>
2021-09-06 03:12:39 -0400

    Agregar archivos de proyecto.

---
+++ Capitulo3/Par.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capitulo3
+{
+    class Par
+    {
+        public static void Evaluar()
+        {
+			int Matricula;
+
+			Console.WriteLine("Ingrece el numero que desee evaluar: ");
+			Matricula = Convert.ToInt32(Console.ReadLine());
+
+			if (Matricula % 2 == 0)
+			{
+				Console.WriteLine("El numero es par.");
+			}
+			else
+			{
+				Console.WriteLine("El numero no es par.");
+			}
+		}
+    }
+}
---
+++ Capitulo3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Capitulo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Par.Evaluar();
+        }
+    }
+}
